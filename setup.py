5766e197bed10a25c8c57854e099e20a4e831ed9
martin-geeks <martin.mt.geek@gmail.com>
2022-01-14 18:20:11 +0200

    completed the setup.py file.

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup
 
 setup(
     name='njugaball',
+    version='0.9.9',
+    description='A web application for playing real lottery.',
+    author='Martin Tembo',
+    author_email='martintembo.zm@gmail.com',
+    url='https://github.com/martin-geeks/njugaball',
     packages=['njugaball'],
     include_package_data=True,
     install_requires=[
         'flask',
         'bcrypt',
         'flask_sqlalchemy',
+        'flask_mail',
         'tea_encrypt',
         'scryp',
         'sqlalchemy_serializer',
